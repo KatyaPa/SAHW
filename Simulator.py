fe8f20060d365e01821b82fa4c26bbdba475c7b9
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-06-24 18:42:01 -0700

    Add reg + Side channel weights

--- Simulator.py
+++ Simulator.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 from pandas import *
 import seaborn as sns
+import webbrowser
 
 
 def clac_math(ifmap,w,psum):
@@ -17,30 +18,44 @@ def calc_str(ifmap, w, psum):
 
     return res
 
+class Register(object):
+
+    def __init__(self, init_val='0'):
+        self.val = init_val
+
+    def step(self, val):
+        self.val = val
+
 class ProcessingElement(object):
     """Basic processing elemnt"""
-    def __init__(self):
-        self.current_weight = '0'
-        self.next_weight    = '0'
-        self.ifmap          = '0'
-        self.psum           = '0'
-        self.out_psum       = '0'
-        self.out_ifmap      = '0'
+    def __init__(self, pe_id):
+        self.cached_weight  = {'odd': Register(), 'even': Register()}
+        self.ifmap          = {'data': Register(), 'epoch': Register('even')}
+        self.psum           = Register()
+        self.weight         = {'addr': Register(), 'data': Register(),
+                               'epoch': Register('even')}
+        self.id             = pe_id
+        self.ifmap_out      = {'data': '0', 'epoch': 'even'}
+        self.weight_out     = {'addr': 0, 'data':'0', 'epoch' : 'even'}
+        self.psum_out       = '0'
 
-    def step(self, in_ifmap, in_psum, calc_fn = calc_str):
+
+    def step(self, ifmap_in, weight_in, psum_in, calc_fn = calc_str):
         ''' Calculation'''
-        self.out_psum  = calc_fn(self.ifmap, self.current_weight, self.psum)
-        self.out_ifmap = self.ifmap
-        ''' Sampling inputs'''
-        self.ifmap = in_ifmap
-        self.psum = in_psum
+        for key in self.ifmap.keys(): self.ifmap_out[key]  = self.ifmap[key].val
+        for key in self.weight.keys(): self.weight_out[key] = self.weight[key].val
+        current_weight = self.cached_weight[self.ifmap['epoch'].val].val
+        self.psum_out   = calc_fn(self.ifmap['data'].val, current_weight, self.psum.val)
 
-    def load_next_weight(self, weight):
-        self.next_weight = weight
+        ''' Sampling'''
+        for key in self.ifmap.keys(): self.ifmap[key].step(ifmap_in[key])
+        for key in self.weight.keys(): self.weight[key].step(weight_in[key])
+        self.psum.step(psum_in)
+        if self.weight['addr'].val == self.id:
+            #import pdb; pdb.set_trace()
+            epoch = self.weight['epoch'].val
+            self.cached_weight[epoch].step(self.weight['data'].val)
 
-    def update_weight(self):
-        self.current_weight = self.next_weight
-        self.next_weight = 0
 
 
 class SystolicArray(object):
@@ -52,35 +67,21 @@ class SystolicArray(object):
         self.sa = np.empty((self.n_rows, self.n_cols), object)
         for i in xrange(self.n_rows):
             for j in xrange(self.n_cols):
-                self.sa[i][j] =  ProcessingElement()
+                self.sa[i][j] =  ProcessingElement(j)
 
-    def step(self, in_ifmaps):
+    def step(self, ifmaps_in, weights_in):
         for i in xrange(self.n_rows):
             for j in xrange(self.n_cols):
-                if i == 0 and j == 0:
-                    self.sa[i][j].step(in_ifmaps[0], 0)
-                elif i == 0:
-                    self.sa[i][j].step(self.sa[i][j-1].out_ifmap, 0)
-                elif j == 0:
-                    self.sa[i][j].step(in_ifmaps[i], self.sa[i-1][j].out_psum)
-                else:
-                    self.sa[i][j].step(self.sa[i][j-1].out_ifmap,
-                                        self.sa[i-1][j].out_psum)
-
-    def load_next_weights(self, weights):
-        for i in xrange(self.n_rows):
-            for j in xrange(self.n_cols):
-                self.sa[i][j].load_next_weight(weights[i][j])
-
-    def update_weights(self):
-        for i in xrange(self.n_rows):
-            for j in xrange(self.n_cols):
-                self.sa[i][j].update_weight()
+                ifmap_in = ifmaps_in[i] if j == 0 else self.sa[i][j-1].ifmap_out
+                psum_in = '0' if i == 0 else self.sa[i-1][j].psum_out
+                weight_in = weights_in[i] if j == 0 else self.sa[i][j-1].weight_out
+                self.sa[i][j].step(ifmap_in, weight_in, psum_in)
 
     def show(self, keys=""):
 
         pandas.set_option('display.max_colwidth', 100)
-        psum_mat = [[sa_e.out_psum for sa_e in sa_row] for sa_row in self.sa]
+        # psum_mat = [[sa_e.cached_weight['even'].val for sa_e in sa_row] for sa_row in self.sa]
+        psum_mat = [[sa_e.psum_out for sa_e in sa_row] for sa_row in self.sa]
         df = DataFrame(psum_mat)
 
         def formatter(v):
@@ -109,16 +110,17 @@ class Data(object):
         self.filters = list()
 
         for i in xrange(n_ifmaps):
-            self.ifmaps.append([str(0)]*i +
-                [''.join(['IF[',str(i),',',str(r),str(c),']'])
+            self.ifmaps.append([{'data':str(0), 'epoch':'even'}]*i +
+                [{'data': ''.join(['IF[',str(i),',',str(r),str(c),']']), 'epoch':'even'}
                     for r in xrange(ifmap_size) for c in xrange(ifmap_size)]
-                                            + [str(0)]*(n_ifmaps-i-1))
+                                            + [{'data':str(0), 'epoch':'even'}]*(n_ifmaps-i-1))
         self.ifmaps = np.array(self.ifmaps)
 
         for i in xrange(n_ifmaps):
             for j in xrange(n_ofmaps):
                 self.filters.append(
-                    [''.join(['W[',str(i),str(j),',',str(r),str(c),']'])
+                    [{'addr':j, 'data':''.join(['W[',str(i),str(j),',',str(r),str(c),']']),
+                    'epoch' :'even'}
                     for r in xrange(filter_size) for c in xrange(filter_size)])
         self.filters = np.array(self.filters)
 
@@ -128,17 +130,26 @@ if __name__ == "__main__":
     if os.path.isfile('some_file.html'):
         os.remove('some_file.html')
 
-    data = Data(5, 7, 5, 3)
-    # weights = [[''.join(['W[',str(i),str(j),',','00]']) for j in xrange(5)] for i in xrange(5)]
-    # print np.array(weights)
-    weights = data.filters[:,0].reshape(5,5)
-    sa = SystolicArray(5,5)
-    sa.load_next_weights(weights)
-    sa.update_weights()
-    keys = data.filters[:,0].tolist()
-    # import pdb; pdb.set_trace()
-    sa.show(keys)
-    for ifmap in data.ifmaps.T:
-        sa.step(ifmap)
-        # print ("IFMAP:", ifmap.tolist())
+    n_ifmaps    = 5
+    ifmap_size  = 7
+    n_ofmaps    = 5
+    filter_size = 3
+
+    data    = Data(n_ifmaps, ifmap_size, n_ofmaps, filter_size)
+    weights = data.filters[:,0].reshape(n_ifmaps,n_ofmaps)
+    sa      = SystolicArray(n_ifmaps,n_ofmaps)
+    keys    = map(lambda x: x['data'], data.filters[:,0])
+    # sa.show(keys)
+
+    ifmap_warmup = [{'data':'0', 'epoch':'even'}]*n_ifmaps
+    print("Warm up phase")
+    for weight in weights.T[::-1]:
+        sa.step(ifmap_warmup, weight)
         sa.show(keys)
+
+    print("Data phase")
+    for ifmap, weight in zip(data.ifmaps.T,weights.T):
+       sa.step(ifmap, weight)
+       sa.show(keys)
+
+    ## webbrowser.open_new('/Users/katia.patkin/MLHW/some_file.html')
